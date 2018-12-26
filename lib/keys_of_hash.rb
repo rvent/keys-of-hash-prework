948cca0f6bc122ced7138a708b94b7f7f4cef0d7
Rene Ventura <venturar729@gmail.com>
2018-12-26 02:02:30 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,12 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
     keys = []
     
+    self.each do |key, value|
+      if value = arguments
+        keys << key
+      end
   end
+  keys
 end
